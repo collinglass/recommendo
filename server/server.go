30996cb524d97bca38feec30bb95d910d8a6edef
Collin Glass <collinglass@gmx.com>
2014-04-05 22:16:43 -0400

    Working Plotter

--- server/server.go
+++ server/server.go
@@ -1,71 +1,50 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
+	"code.google.com/p/plotinum/plot"
+	"code.google.com/p/plotinum/plotter"
+	//"code.google.com/p/plotinum/plotutil"
+	"github.com/collinglass/recommendo/data"
 	"github.com/collinglass/recommendo/reco"
-	"github.com/gorilla/mux"
 	"log"
-	"net/http"
 )
 
-func RecommendationHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
+func main() {
+	log.Println("Starting Algorithms...")
 
-	ureco, ureco2 := reco.UserRunner()
-	ireco, ireco2 := reco.ItemRunner()
+	ureco, _ := reco.UserRunner()
+	//ireco, ireco2 := reco.ItemRunner()
+	scatterData := createPlot(ureco)
 
-	slc := make([][2]interface{}, 0, 50)
-	slcval := [2]interface{}{"Book", "Rating"}
-	slc = append(slc, slcval)
-	for _, val := range ureco {
-		slcval = [2]interface{}{val.Book, val.Score}
-		slc = append(slc, slcval)
-	}
-
-	slc2 := make([][2]interface{}, 0, 50)
-	slcval = [2]interface{}{"Book", "Rating"}
-	slc2 = append(slc2, slcval)
-	for _, val := range ureco2 {
-		slcval = [2]interface{}{val.Book, val.Score}
-		slc2 = append(slc2, slcval)
-	}
-
-	slc3 := make([][2]interface{}, 0, 50)
-	slcval = [2]interface{}{"Book", "Rating"}
-	slc3 = append(slc3, slcval)
-	for _, val := range ireco {
-		slcval = [2]interface{}{val.Book, val.Score}
-		slc3 = append(slc3, slcval)
-	}
-
-	slc4 := make([][2]interface{}, 0, 50)
-	slcval = [2]interface{}{"Book", "Rating"}
-	slc4 = append(slc4, slcval)
-	for _, val := range ireco2 {
-		slcval = [2]interface{}{val.Book, val.Score}
-		slc4 = append(slc4, slcval)
-	}
-
-	metaslc := [4]interface{}{slc, slc2, slc3, slc4}
-
-	json, err := json.Marshal(metaslc)
+	p, err := plot.New()
 	if err != nil {
-		fmt.Println(err)
+		panic(err)
 	}
 
-	w.Write([]byte(json))
+	p.Title.Text = "User: Book Ratings"
+	p.X.Label.Text = "Book"
+	p.Y.Label.Text = "Rating"
+
+	s, err := plotter.NewScatter(scatterData)
+	if err != nil {
+		panic(err)
+	}
+	p.Add(s)
+
+	// Save the plot to a PNG file.
+	if err := p.Save(4, 4, "points.png"); err != nil {
+		panic(err)
+	}
+	log.Println("Finishing Algorithms...")
 }
 
-func main() {
-	log.Println("Starting Server")
-
-	r := mux.NewRouter()
-	r.HandleFunc("/api/recommendation", RecommendationHandler).Methods("GET")
-	http.Handle("/api/", r)
-
-	http.Handle("/", http.FileServer(http.Dir("../app/")))
-
-	log.Println("Listening on 8080")
-	http.ListenAndServe(":8080", nil)
+func createPlot(input map[int]data.Recommendation) plotter.XYs {
+	pts := make(plotter.XYs, len(input))
+	j := 0
+	for i := range input {
+		pts[j].X = float64(input[i].Book)
+		pts[j].Y = input[i].Score
+		j++
+	}
+	return pts
 }
